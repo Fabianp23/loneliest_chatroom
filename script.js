fdb4d1b2fb4f3461673dec36bff40f15dab25571
Fabian Pombo <contact@fabianpombo.com>
2016-05-07 15:53:41 -0400

    send button appends message to chat box

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
   $("#new-message-button").on('click', function(){
     console.log("The button works");
-  });
+    $('#conversation').append($("#new-message-body").val());
+      console.log("Message Sent");
+  })
 
 });
