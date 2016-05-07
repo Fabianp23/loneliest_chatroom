faada6dd88e3a146a63fdc8294ea919b38b66d6c
Fabian Pombo <contact@fabianpombo.com>
2016-05-07 18:24:53 -0400

    added time stamp in non-military hours feature

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ $(document).ready(function(){
 
   function sendMethod(){
     var inputText = $("#new-message-body").val();
+
+    var getDate = new Date;
+    var timeStampInHours = getDate.getHours()
+    function timeFixer(t){
+      if (t >= 12 ) {
+        return t - 12 + ": " + getDate.getMinutes() + "pm";
+      } else {
+        return t + ": " + getDate.getMinutes() + "am";
+      };
+    }
+
     var newMessage = "<li class='message'>";
         newMessage += "<a class='delete' href='#'>Delete</a>";
         newMessage += "<h3 class='author'>Me</h3>";
         newMessage += "<p class='new-message-body'>";
         newMessage += inputText;
-        newMessage +="</p>";
-        newMessage += "<span class='timestamp'></span";
+        newMessage += "</p>";
+        newMessage += "<span class=timestamp>"
+        newMessage += timeFixer(timeStampInHours);
+        newMessage += "</span";
         newMessage += "</li>";
-    $('#conversation').append(newMessage);
+
+    $('#conversation').append(newMessage)
       // console.log("Message Sent");
     $('.delete').on('click', function(){
       // console.log("x(button) is click-able");
@@ -28,5 +42,5 @@ $(document).ready(function(){
       sendMethod();
     }
   });
-  
+
 });
