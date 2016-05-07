8a3366915c5520529dc021ac31c27313650de425
Fabian Pombo <contact@fabianpombo.com>
2016-05-07 17:08:32 -0400

    added submit messages with enter feature

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 
   $("#new-message-button").on('click', function(){
     console.log("The button works");
-
     var inputText = $("#new-message-body").val();
     var newMessage = "<li class='message'>";
         newMessage += "<a class='delete' href='#'>Delete</a>";
@@ -12,14 +11,38 @@ $(document).ready(function(){
         newMessage +="</p>";
         newMessage += "<span class='timestamp'></span";
         newMessage += "</li>";
-
     $('#conversation').append(newMessage);
       console.log("Message Sent");
-
     $('.delete').on('click', function(){
       console.log("x(button) is click-able");
       $(this).parent().remove();
+      console.log("message was deleted");
     });
-
   });
+
+  $("#new-message-body").keypress(function(e) {
+    var inputText = $("#new-message-body").val();
+    var newMessage = "<li class='message'>";
+        newMessage += "<a class='delete' href='#'>Delete</a>";
+        newMessage += "<h3 class='author'>Me</h3>";
+        newMessage += "<p class='new-message-body'>";
+        newMessage += inputText;
+        newMessage +="</p>";
+        newMessage += "<span class='timestamp'></span";
+        newMessage += "</li>";
+    if (e.which == 13) {
+      $('#conversation').append(newMessage);
+      console.log("Enter Key Works");
+      console.log("Message Sent");
+      $('.delete').on('click', function(){
+        console.log("x(button) is click-able");
+        $(this).parent().remove();
+        console.log("message was deleted");
+      });
+    }
+  });
+
+
+
+
 });
