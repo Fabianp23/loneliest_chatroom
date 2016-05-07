138b884520ed18d749da6e9bd9fd64708afd5947
Fabian Pombo <contact@fabianpombo.com>
2016-05-07 16:12:03 -0400

    full message box appends instead of the text by itself

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ $(document).ready(function(){
 
   $("#new-message-button").on('click', function(){
     console.log("The button works");
-    $('#conversation').append($("#new-message-body").val());
-      console.log("Message Sent");
-  })
 
+    var inputText = $("#new-message-body").val();
+    var newMessage = "<li class='message'>";
+        newMessage += "<a class='delete' href='#'>Delete</a>";
+        newMessage += "<h3 class='author'>Me</h3>";
+        newMessage += "<p class='new-message-body'>";
+        newMessage += inputText;
+        newMessage +="</p>";
+        newMessage += "span class='timestamp'></span";
+        newMessage += "</li>";
+
+    $('#conversation').append(newMessage);
+      console.log("Message Sent");
+
+  });
 });
